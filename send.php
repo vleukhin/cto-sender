ae355b2c664e86b7cba5996dcc23e4d5d572a688
vleukhin <norad-2013@mail.ru>
2017-12-06 16:55:29 +0700

    сохраняем utm метки

--- send.php
+++ send.php
@@ -4,8 +4,9 @@ require 'vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv('./');
 $dotenv->load();
+$utm_service = new \App\UtmService();
 
-$lead = new \App\Lead($_POST['data']);
+$lead = new \App\Lead($_POST['data'] + $utm_service->getUtms());
 
 $manager = new \App\LeadsManager();
 $manager->pushSender(new \App\Senders\CollectorSender(getenv('COLLECTOR_HOST')));
