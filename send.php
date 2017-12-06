f093faba8fc328c4fa1687e0b09fcff485d47e5a
vleukhin <norad-2013@mail.ru>
2017-12-06 13:18:14 +0700

    test geo

--- send.php
+++ send.php
@@ -6,3 +6,10 @@ $dotenv = new Dotenv\Dotenv('./');
 $dotenv->load();
 
 echo getenv('COLLECTOR_URL');
+
+$geo = new \App\Geo([
+    'id'      => $_SERVER['REMOTE_ADDR'],
+    'charset' => 'utf-8',
+]);
+
+var_dump($geo->get_value(false, false));
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Dotenv\\' => array($vendorDir . '/vlucas/phpdotenv/src'),
+    'App\\' => array($baseDir . '/app'),
 );
