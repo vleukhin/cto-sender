12409ec3bd71c00c66c55537d8923c68e4ca40ca
vleukhin <norad-2013@mail.ru>
2017-12-06 17:15:34 +0700

    mail sender

--- app/Senders/EmailSender.php
+++ app/Senders/EmailSender.php
@@ -12,17 +12,57 @@ use App\Lead;
 
 class EmailSender implements Sender
 {
+    /**
+     * С какой почты отправлять
+     *
+     * @var string
+     */
+    protected $email_from;
+
+    /**
+     * На какую почту отправлять
+     *
+     * @var string
+     */
+    protected $email_to;
+
+    /**
+     * Тема письма
+     *
+     * @var string
+     */
+    protected $subject;
+
+    /**
+     * Имя отправителя
+     *
+     * @var string
+     */
+    protected $name;
+
+    public function __construct($email_from, $email_to, $name, $subject)
+    {
+        $this->email_from = $email_from;
+        $this->email_to = $email_to;
+        $this->subject = $subject;
+        $this->name = $name;
+    }
 
     /**
      * Отправка лида
      *
      * @param Lead $lead
      *
-     * @return mixed
+     * @return bool
      */
     public function send(Lead $lead)
     {
-        // TODO: Implement send() method.
+        $text = $lead->toString();
+
+        $headers = "MIME-Version: 1.0\r\nContent-type: text/html; charset=utf-8\r\n";
+        $headers .= "From: =?UTF-8?B?" . base64_encode($this->name) . "?= <" . $this->email_from . ">\r\n";
+
+        return mail($this->email_to, "=?UTF-8?B?" . base64_encode($this->subject) . "?=", $text, $headers);
     }
 
     /**
