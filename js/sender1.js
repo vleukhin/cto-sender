78bd3744b2c479eda4caa485467ca92675f2040e
vleukhin <norad-2013@mail.ru>
2018-02-13 16:28:10 +0700

    проверка номера в скрипте сендера

--- js/sender1.js
+++ js/sender1.js
@@ -3,10 +3,39 @@
  * Tel: +7-926-797-5419
  * E-mail: vleukhin@ya.ru
  */
+
+function validPhone(phone) {
+    var pattern = /^((8|\+7|7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{10}$/;
+
+    return pattern.test(phone);
+}
+
 (function ($) {
     $(document).ready(function () {
+        var body = $('body');
+        body.delegate('[type="submit"]', 'click', function () {
+            var form = $(this).closest('form');
+            var field = form.find('[name=phone]');
+            var phone = field.val();
+
+            if (!validPhone(phone)) {
+                field.attr('title', 'Номер введен некорректно!');
+
+                if (!field.hasClass('tooltipstered')) {
+                    field.tooltipster();
+                }
+
+                field.tooltipster('open');
+                field.focus();
+
+                return false;
+            }
+
+            return true;
+        });
+
         var loading = false;
-        $('body').delegate('form.sform', 'submit', function (event) {
+        body.delegate('form.sform', 'submit', function (event) {
             event.preventDefault();
 
             if (!loading) {
