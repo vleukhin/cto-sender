168abaa852323a8fdc672f2d8c01e8aa53307575
Viktor <norad-2013@mail.ru>
2018-03-19 15:29:59 +0700

    удаляем файл с курса каждый день

--- currencies.php
+++ currencies.php
@@ -4,16 +4,25 @@ require 'vendor/autoload.php';
 
 $file = __DIR__ . '/storage/rates.json';
 
+$now = time();
+
 $service = new \App\CurrencyService();
 if (file_exists($file)) {
-
 	$rates = @json_decode(file_get_contents($file), true);
+
+	if (empty($rates['expired']) or $rates['expired'] < $now) {
+		unlink($file);
+		unset($rates);
+	}
 }
+
 
 if (empty($rates)) {
 	$rates = $service->getRates(['USD', 'EUR'])
 		->to('RUB')
 		->fromCBRF();
+
+	$rates['expired'] = strtotime('tomorrow', $now) - 1;
 
 	file_put_contents($file, json_encode($rates));
 }
