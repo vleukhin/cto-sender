3e027691ec43cdf789f15294cd28fd35d1e9b2dc
Viktor <norad-2013@mail.ru>
2018-03-19 15:21:31 +0700

    сохраняем куры в файл, что бы не обращаться к сервису каждый раз

--- currencies.php
+++ currencies.php
@@ -2,12 +2,25 @@
 
 require 'vendor/autoload.php';
 
+$file = __DIR__ . '/storage/rates.json';
+
 $service = new \App\CurrencyService();
-$rates = $service->getRates(['USD', 'EUR'])->to('RUB')->fromCBRF();
+if (file_exists($file)) {
+
+	$rates = @json_decode(file_get_contents($file), true);
+}
+
+if (empty($rates)) {
+	$rates = $service->getRates(['USD', 'EUR'])
+		->to('RUB')
+		->fromCBRF();
+
+	file_put_contents($file, json_encode($rates));
+}
 
 function convert_currency($amount, $from)
 {
 	global $rates;
 
-	return number_format($amount * $rates[$from]['RUB'], 0);
+	return number_format($amount * $rates[$from]['RUB'], 0, ',', '');
 }
